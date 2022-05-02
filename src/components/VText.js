c22cf8eb9d15ee5b9934e60d0b5d7c9b0231f73f
theafolayan <theafolayan@gmail.com>
2022-05-02 12:49:14 +0100

    defined basic props for Vtext component

--- src/components/VText.js
+++ src/components/VText.js
@@ -1,12 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-export default function VText() {
+export default function VText({
+  children,
+  title,
+  dark,
+  centered,
+  small,
+  grey,
+  primary,
+  red,
+  green,
+  left,
+  medium,
+  white,
+  bold,
+  padded,
+  ...props
+}) {
   return (
-    <View>
-      <Text>VText</Text>
-    </View>
-  )
+    <Text
+      style={[
+        styles.text,
+        centered && styles.centered,
+        padded && styles.padded,
+        title && styles.title,
+        small && styles.small,
+        grey && styles.grey,
+        red && styles.red,
+        green && styles.green,
+        primary && styles.primary,
+        medium && styles.medium,
+        left && styles.left,
+        white && styles.white,
+        bold && styles.bold
+      ]}
+    >
+      {children}
+    </Text>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    text: {}
+});
