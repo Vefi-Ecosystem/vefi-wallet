9bb2972ed3aeefab160c30ae7ed89f90db80fb76
theafolayan <theafolayan@gmail.com>
2022-05-03 22:31:18 +0100

    done with basic wallet backup screen

--- src/screens/onboarding/WalletBackup.js
+++ src/screens/onboarding/WalletBackup.js
@@ -1,10 +1,14 @@
 import { View } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import Screen from '../../components/common/Screen';
 import VText from '../../components/common/VText';
 import VButton from '../../components/Button/Button';
+import VRadio from '../../components/Input/VRadio';
 
 export default function WalletBackup() {
+  const [radioOneChecked, setRadioOneChecked] = useState(false);
+  const [radioTwoChecked, setRadioTwoChecked] = useState(false);
+  const [radioThreeChecked, setRadioThreeChecked] = useState(false);
   return (
     <Screen>
       <View style={{ flex: 1, justifyContent: 'space-between' }}>
@@ -12,14 +16,32 @@ export default function WalletBackup() {
           <VText medium font="GilroySemiBold" centered>
             Backup Your Wallet
           </VText>
-          <VText font="GilroyMedium" small centered>
+          <VText font="GilroyMedium" small centered grey>
             As you proceed you will see secret phrase that allows you recover your wallet.
           </VText>
         </View>
 
-              <View style={{ marginVertical: 30 }}>
-                  
-                  
+        <View style={{ marginVertical: 30 }}>
+          <VRadio
+            checked={radioOneChecked}
+            onPress={() => {
+              setRadioOneChecked(!radioOneChecked);
+            }}
+          />
+
+          <VRadio
+            checked={radioTwoChecked}
+            onPress={() => {
+              setRadioTwoChecked(!radioTwoChecked);
+            }}
+          />
+          <VRadio
+            checked={radioThreeChecked}
+            onPress={() => {
+              setRadioThreeChecked(!radioThreeChecked);
+            }}
+          />
+
           <VButton label="Continue" />
         </View>
       </View>
