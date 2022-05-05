943b8ac4372eed34b029da13ae5c70d6bafc0692
theafolayan <theafolayan@gmail.com>
2022-05-05 08:34:49 +0100

    started work on secretphrase screen

--- src/navigation.js
+++ src/navigation.js
@@ -5,17 +5,21 @@ import Onboarding from './screens/onboarding/Onboarding';
 import EnableNotifications from './screens/onboarding/EnableNotifications';
 import WalletBackup from './screens/onboarding/WalletBackup';
 import Settings from './screens/auth/settings/Settings';
+import SecretPhrase from './screens/onboarding/SecretPhrase';
 
 const { Group, Navigator, Screen } = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <Navigator initialRouteName="settings">
+      <Navigator initialRouteName="secretPhrase">
         <Group screenOptions={{ headerShown: false }}>
+          {/* {Onboarding} */}
+
           <Screen name="onboarding" component={Onboarding} />
           <Screen name="walletBackup" component={WalletBackup} />
           <Screen name="notifications" component={EnableNotifications} />
+          <Screen name="secretPhrase" component={SecretPhrase} />
 
           {/* SETTINGS */}
           <Screen name="settings" component={Settings} />
---
+++ src/screens/onboarding/SecretPhrase.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Screen from '../../components/common/Screen';
+
+export default function SecretPhrase() {
+  return (
+    <Screen backButtonShown>
+      <Text>SecretPhrase</Text>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({});
